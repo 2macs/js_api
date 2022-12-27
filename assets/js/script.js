cb557f10df5267fd38c949e2fcd595f4663ccabd
2macs <adrian.mcneill@outlook.com>
2022-12-27 18:44:17 +0000

    final version

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,15 +6,56 @@ const resultsModal = new bootstrap.Modal(document.getElementById('resultsModal')
 document.getElementById('status').addEventListener('click', e => getStatus(e));
 document.getElementById('submit').addEventListener('click', e => postForm(e));
 
+
+function processOptions(form) {
+    let optArray = [];
+
+    for (let e of form.entries()) {
+        if (e[0] === "options") {
+            optArray.push(e[1]);
+        }
+    }
+
+    form.delete("options");
+
+    form.append("options", optArray.join());
+
+    return form;
+}
+
 async function postForm(e){
-    const form = new FormData(document.getElementById('checksform'));
+    const form = processOptions(new FormData(document.getElementById("checksform")));
     const response = await fetch(API_URL,{
         method:'POST',
         headers:{
             'Authorization': API_KEY,
         },
         body: form,
-    })
+    });
+    const data = await response.json(); //turn the data into JSON to prep for posting //
+    if (response.ok){
+        displayErrors(data);
+    } else {
+        displayException(data);
+        throw new Error(data.error);
+    }
+}
+
+function displayErrors(data){
+    let heading = `JShint results for ${data.file}`;
+    if(data.total_errors === 0){
+        results = `<div class='no_errors'>No errors recorded, great work!</div>`;
+    } else {
+        results = `<div>Total errors: <span class="error_count">${data.total_errors}</span></div>`;
+        for (let error of data.error_list) {
+            results += `<div>At line <span class="line">${error.line}</span>, `;
+            results += `column <span class="column">${error.col}:</span></div>`;
+            results += `<div class="error">${error.error}</div>`;
+        }
+    }
+    document.getElementById("resultsModalTitle").innerText = heading; //set the modal heading //
+    document.getElementById("results-content").innerHTML = results; //set the modal content //
+    resultsModal.show(); //display the modal //
 }
 
 async function getStatus(e) {
@@ -24,7 +65,9 @@ async function getStatus(e) {
     const data = await response.json();
     if (response.ok){  //if no issue with the fetch command then response.ok property is set to true //
         displayStatus(data);  //call displaystatus function //
-    } else throw new Error(data.error);
+    } else 
+        displayException(data);
+        throw new Error(data.error);
 }
 
 function displayStatus(data) {
@@ -35,4 +78,17 @@ function displayStatus(data) {
     document.getElementById('results-content').innerHTML = results;
 
     resultsModal.show();
+}
+
+function displayException(data){
+    let heading = `An exception occurred`;
+    results = `<div>The API returned status code ${data.status_code}</div>`;
+    results += `<div>Error no.: <strong>${data.error_no}</strong></div>`;
+    results += `<div>Error text: <strong>${data.error}</strong></div>`;
+
+    document.getElementById('resultsModalTitle').innerText = heading;
+    document.getElementById('results-content').innerHTML = results;
+
+    resultsModal.show();
+
 }
