ad81baf7bc7bcafcc0e32e1f20d64932e8c38fba
2macs <adrian.mcneill@outlook.com>
2022-12-27 12:01:15 +0000

    add API key details

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+const API_KEY = 'qpWZx4ctmYlHlmeYXG9QKJ6YdCA';
+const API_URL = 'https://ci-jshint.herokuapp.com/api?api_key=qpWZx4ctmYlHlmeYXG9QKJ6YdCA';
+const resultsModal = new bootstrap.Modal(document.getElementById('results-modal'));
+
+//add an event listener for a button//
+document.getElementById('status').addEventListener('click', e=> getStatus(e));
+
+async function getStatus(e) {
+
+
+}
