8cb91f1a1ceb2837960965418713abaee45a5716
2macs <adrian.mcneill@outlook.com>
2022-12-27 13:44:10 +0000

    add fetch statement, verify its coming in correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,16 @@
 const API_KEY = 'qpWZx4ctmYlHlmeYXG9QKJ6YdCA';
-const API_URL = 'https://ci-jshint.herokuapp.com/api?api_key=qpWZx4ctmYlHlmeYXG9QKJ6YdCA';
-const resultsModal = new bootstrap.Modal(document.getElementById('results-modal'));
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById('resultsModal'));
 
 //add an event listener for a button//
 document.getElementById('status').addEventListener('click', e=> getStatus(e));
 
 async function getStatus(e) {
-
-
+    const queryString = `${API_URL}?api_key=${API_KEY}`;  //follow format required by api documentation //
+    //fetch the query and assign it to response //
+    const response = await fetch(queryString);
+    const data = await response.json();
+    if (response.ok){  //if no issue with the fetch command then response.ok property is set to true //
+        console.log(data.expiry);  // logs the repsonse to the console //
+    }
 }
